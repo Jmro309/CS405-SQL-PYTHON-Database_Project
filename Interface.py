33c20e3af0d641c99fbe1187eb32b419a6fb71f1
Jmro309 <jmro309@uky.edu>
2026-04-27 00:49:39 -0400

    Update Functionality for Manage Faculty
    Added A list advisor function returns a list of advisors names and IDs
    
    Added a function to view advised clubs that displays all of the clubs that a faculty member advises
    
    Finished the logic and implementation for  Manage faculty function

--- Interface.py
+++ Interface.py
@@ -39,6 +39,23 @@ def list_clubs(connection):         # Make a list of the names and years of all 
     cursor.close()
     return clubs  # return the full list of clubs
 
+def list_faculty(connection):       # Make a list of the names and ID's if all faculty
+    cursor = connection.cursor()
+    cursor.execute("SELECT name,facultyID FROM faculty ORDER BY name ASC")
+    faculty_list = cursor.fetchall()
+
+    if not faculty_list:
+        print("No Faculty members found.")
+        cursor.close()
+        return[]
+    
+
+    for i, row in enumerate(faculty_list, start = 1):
+        print(f"{i}. {row[0]} (ID: {row[1]})")
+    
+    cursor.close()
+    return faculty_list
+
 # ====================================== Make functions for each of the 3 main objectives of the project ====================================== #
 
 def manage_clubs(connection):               # 1. Manage clubs and their associated information
@@ -465,8 +482,44 @@ def report_budget(connection, club_name, club_year):
 
 def manage_faculty(connection):             # 2. Manage faculty members who advise clubs
     while True:
-        break
+        print(f"\n=== Manage Faculty ===")
+        print("Select a Faculty Member to View Advised Clubs")
 
+        faculty = list_faculty(connection)
+
+        print(f"{len(faculty) + 1}. Return\n ")
+
+        choice = get_menu_choice(1, len(faculty) +1 )
+        if choice == len(faculty) + 1:
+            break;
+        
+        selected_faculty = faculty[choice - 1]
+        faculty_name = selected_faculty[0]
+        faculty_ID = selected_faculty[1]
+
+        view_advised_clubs(connection,faculty_ID,faculty_name)
+
+
+        
+
+def view_advised_clubs(connection, faculty_ID, faculty_name): #Display all of the clubs that a facutly member advises
+        cursor = connection.cursor()
+        cursor.execute("""
+            SELECT clubName,clubYear 
+            FROM advisor
+            WHERE advisor.facultyID = %s """, (faculty_ID,) )
+        
+        result = cursor.fetchall()
+        
+        print(f"\n=== Clubs Advised by {faculty_name} ===")
+        if result:
+            for row in result:
+                print("Club Name: ,{row[0]} ({row[1]})")
+        else:
+            print("Advises no clubs.")
+
+        cursor.close()
+        input("\n Press Enter to continue...")
 def manage_students(connection):            # 3. Manage students and their club memberships
     while True:
         break
